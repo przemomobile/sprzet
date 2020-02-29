4d645c82894aaf942e85717805abfcdc4dd35ce7
przemomobile <Przemek@przemek>
2020-02-29 13:50:00 +0100

    Kasacja

--- src/com/company/pilka_nozna.java
+++ src/com/company/pilka_nozna.java
@@ -1,14 +1,2 @@
 package com.company;
 
-public class pilka_nozna extends gry_zespolowe implements uzyj{
-
-    @Override
-    public boolean uzyjWPilceNoznej() {
-        return false;
-    }
-
-        @Override
-    public boolean uzyjWSiatkowce() {
-        return false;
-    }
-}
